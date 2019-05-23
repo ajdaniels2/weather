74398ad9b11a20531ea003bd30d3bec3da13a570
ajdaniels2 <ajdanielsjr@gmail.com>
2019-05-23 19:05:56 -0400

    first commit

---
+++ src/Components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3 className="title-container__subtitle">I’m using a free version of openweathermap.org to fetch the API. So, the only city and country that will fetch is London, GB.</h3>
+    </div>   
+);
+
+export default Titles;
