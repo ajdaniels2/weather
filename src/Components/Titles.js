46c4aab5b14696f9e6443159c7c80fe84121e10e
ajdaniels2 <49796201+ajdaniels2@users.noreply.github.com>
2019-05-30 21:16:32 -0400

    Update Titles.js

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -3,7 +3,7 @@ import React from "react";
 const Titles = () => (
     <div>
         <h1 className="title-container__title">Weather Finder</h1>
-        <h3 className="title-container__subtitle">I’m using a free version of openweathermap.org to fetch the API. So, the only city and country that will fetch is London, GB.</h3>
+        <h3 className="title-container__subtitle">I’m using a free version of openweathermap.org to fetch the API. So, the only city and country that will load is London, GB.</h3>
     </div>   
 );
 
